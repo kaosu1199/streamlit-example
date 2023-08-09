4b3f34b30c3f7144aab7ca4d33bb2cda4d4e8818
IS-TANABE\tanab <tanabe@initialsite.com>
2023-08-09 15:52:01 +0900

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,27 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+from langchain.llms import OpenAI
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.memory import ConversationBufferMemory
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+llm = OpenAI(openai_api_key = "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" , model_name="text-davinci-003", temperature=0.2)
+tool_names = ["google-search"]
+tools = load_tools(tool_names, llm=llm , google_api_key="AIzaSyAXpGFUZOkgf8-ZWhyoO539zBKFSRgFsSk" , google_cse_id = "f2ba75d17d5a04cf9")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True,
+)
 
-    points_per_turn = total_points / num_turns
+agent = initialize_agent(
+    tools=tools,
+    llm=llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True
+)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+agent.run("""
+現在の日本の総理大臣は誰ですか？
+""")
